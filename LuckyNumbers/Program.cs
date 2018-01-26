c990234f0898a8dd0aa94f558527889fcc69ab4c
PDunipace22 <peterdunipace@gmail.com>
2018-01-26 14:18:46 -0500

    variables and random number generator

--- LuckyNumbers/Program.cs
+++ LuckyNumbers/Program.cs
@@ -10,6 +10,32 @@ namespace LuckyNumbers
     {
         static void Main(string[] args)
         {
+            int lowNum = 0;
+            int highNum = 0;
+            Console.WriteLine("Please select the lowest number.");
+            lowNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please select the highest number.");
+            highNum = int.Parse(Console.ReadLine());
+            //int[] pickNum = new int[6];
+            //for (int i = 0; i < pickNum.Length; i++)
+            //{
+            //    Console.WriteLine("Please pick six numbers.");
+            //    if (pickNum[i] > lowNum && pickNum[i] < highNum)
+            //    {
+            //        pickNum[i] = int.Parse(Console.ReadLine());
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Please pick another number.");
+            //    }
+
+            Random drawingNum = new Random();
+            int[] number = new int[6];
+            for(int i = 0; i < number.Length; i++)
+            {
+                number[i] = drawingNum.Next(lowNum, highNum);
+                Console.WriteLine("Lucky number: " + number[i]);
+            }
+            }
         }
     }
-}
