b78ebed1090aaa0154c9af63defd4749220fc35e
PDunipace22 <peterdunipace@gmail.com>
2018-01-27 22:43:37 -0500

    final commit

--- LuckyNumbers/Program.cs
+++ LuckyNumbers/Program.cs
@@ -10,51 +10,98 @@ namespace LuckyNumbers
     {
         static void Main(string[] args)
         {
-            int jackPot = 1000000;
-            Console.WriteLine("The jack pot is $" + jackPot + " dollars.");
-            int lowNum = 0;
-            int highNum = 0;
-            Console.WriteLine("Please select the lowest number.");
-            lowNum = int.Parse(Console.ReadLine());
-            Console.WriteLine("Please select the highest number.");
-            highNum = int.Parse(Console.ReadLine());
-            int[] pickNum = new int[6];
-            bool dupes = false;
-            for (int i = 0; i < pickNum.Length; i++)
+            int jackPot = 3000000;
+            Console.WriteLine("The jack pot is {0:c}", jackPot);
+            do
             {
-                Console.WriteLine("Please pick a number ({0}).", i+1);
-                do
+                int lowNum = 0;
+                int highNum = 0;
+                Console.WriteLine("Please select the lowest number.");
+                lowNum = int.Parse(Console.ReadLine());
+                Console.WriteLine("Please select the highest number.");
+                highNum = int.Parse(Console.ReadLine());
+                int[] pickNum = new int[6];
+                bool dupes = false;
+                for (int i = 0; i < pickNum.Length; i++)
                 {
-                    dupes = false;
-                    pickNum[i] = int.Parse(Console.ReadLine());
-                    if (pickNum[i] < lowNum || pickNum[i] > highNum)
+                    Console.WriteLine("Please pick a number ({0}).", i + 1);
+                    do
                     {
-                        Console.WriteLine("Number out of range. Please pick another number ({0}).", i+1);
-                    }
-                    else
-                    {
-                        for (int j = 0; j < i; j++)
+                        dupes = false;
+                        pickNum[i] = int.Parse(Console.ReadLine());
+                        if (pickNum[i] < lowNum || pickNum[i] > highNum)
                         {
-                            if (pickNum[i] == pickNum[j])
+                            Console.WriteLine("Number out of range. Please pick another number ({0}).", i + 1);
+                        }
+                        else
+                        {
+                            for (int j = 0; j < i; j++)
                             {
-                                dupes = true;
-                                Console.WriteLine("Duplicate number. Please pick another number ({0}).", i+1);
+                                if (pickNum[i] == pickNum[j])
+                                {
+                                    dupes = true;
+                                    Console.WriteLine("Duplicate number. Please pick another number ({0}).", i + 1);
+                                }
                             }
                         }
-                    }
-                } while (pickNum[i] < lowNum || pickNum[i] > highNum || dupes);
-                
-            }
-            Random drawingNum = new Random();
-            int[] number = new int[6];
-            for (int i = 0; i < number.Length; i++)
-            {
-                number[i] = drawingNum.Next(lowNum, highNum);
-                Console.WriteLine("Lucky number: " + number[i]);
-            }
-            int totalWon = (jackPot / 6);
-            Console.WriteLine("You won $" + " " + totalWon + " " + "!");
+                    } while (pickNum[i] < lowNum || pickNum[i] > highNum || dupes);
+                }
+                Random drawingNum = new Random();
+                int[] number = new int[6];
+                for (int i = 0; i < number.Length; i++)
+                {
+                    do
+                    {
+                        dupes = false;
+                        number[i] = drawingNum.Next(lowNum, highNum + 1);
+                        for (int j= 0; j < i; j++)
+                        {
+                            if(number[i] == number[j])
+                            {
+                                dupes = true;
+                            }
+                        }
+                    } while (dupes);
+                    Console.WriteLine("Lucky number: " + number[i]);
+                }
+                int match = MatchCount(pickNum, number);
+                Console.WriteLine("You correctly matched {0} numbers.", match);
+                double totalWon = (jackPot / 6) * match;
+                Console.WriteLine("You won {0:c}!", totalWon);
+            } while (Play());
+            Console.WriteLine("Thanks for playing!");
         }
+
+        static int MatchCount(int[] pickNum,int[] number)
+        {
+            int counter = 0;
+            for(int p = 0; p < pickNum.Length; p++)
+            {
+                for(int n = 0; n < number.Length; n++)
+                {
+                    if (pickNum[p] == number[n])
+                    {
+                        counter++;
+                    }
+                }
+            }
+            return counter;
+        }
+
+        static bool Play()
+        {
+            Console.WriteLine("Do you wish to play again?");
+            string playAgain = Console.ReadLine();
+            if(playAgain.ToLower() == "yes")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
     }
 }
             
